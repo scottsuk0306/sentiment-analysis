5ad88d283e4efe9753d97365a9ab024e4f1ba46b
scottsuk0306 <scottsuk0306@kaist.ac.kr>
2022-07-08 02:43:36 +0900

    Preprocess

---
+++ src/preprocess.py
@@ -0,0 +1,48 @@
+import re
+from bs4 import BeautifulSoup
+import nltk
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+#Removing the html strips
+def strip_html(text):
+    soup = BeautifulSoup(text, "html.parser")
+    return soup.get_text()
+
+#Removing the square brackets
+def remove_between_square_brackets(text):
+    return re.sub('\[[^]]*\]', '', text)
+
+#Removing the noisy text
+def denoise_text(text):
+    text = strip_html(text)
+    text = remove_between_square_brackets(text)
+    return text
+
+#Define function for removing special characters
+def remove_special_characters(text, remove_digits=True):
+    pattern=r'[^a-zA-z0-9\s]'
+    text=re.sub(pattern,'',text)
+    return text
+
+#Stemming the text
+def simple_stemmer(text):
+    ps=nltk.porter.PorterStemmer()
+    text= ' '.join([ps.stem(word) for word in text.split()])
+    return text
+
+#removing the stopwords
+def remove_stopwords(text, is_lower_case=False):
+    # Tokenization of text 
+    tokenizer = ToktokTokenizer()
+    # Setting Enlgihs stopwords 
+    stopword_list = nltk.corpus.stopwords.words('english')
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    if is_lower_case:
+        filtered_tokens = [token for token in tokens if token not in stopword_list]
+    else:
+        filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)    
+    return filtered_text
---
+++ src/train.py
@@ -0,0 +1,4 @@
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
